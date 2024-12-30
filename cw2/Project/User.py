463c2d861f09db107f14ce26251736c477b1290b
Corey Richardson <richardsoncorey04@gmail.com>
2024-12-30 16:46:12 +0000

    `User` CRUD methods

---
+++ cw2/Project/User.py
@@ -0,0 +1,58 @@
+from authentication import authenticate
+from config import db
+from models import User, UserSchema, Trail, TrailSchema, Point, PointSchema
+
+from flask import abort, make_response
+
+def create(user):
+    required_fields = ["email", "role"]
+    missing_fields = [field for field in required_fields if not user.get(field)]
+    if missing_fields:
+        abort(400, f"Missing required fields: {', '.join(missing_fields)}")
+        
+    try:
+        new_user = UserSchema().load(user, session = db.session)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        abort(500, f"Error creating new user: {str(e)}")
+    
+    return UserSchema().dump(new_user), 201
+
+
+def read_one(id):
+    user = User.query.get(id)
+    
+    if user is not None:
+        return UserSchema().dump(user), 200
+    
+    abort(404, f"User with ID {id} not found.")
+    
+
+def read_all():
+    users = User.query.all()
+    return UserSchema(many = True).dump(users), 200
+
+
+def update(id, user):
+    existing_user = User.query.get(id)
+    
+    if existing_user:
+        for key, value in user.items():
+            setattr(existing_user, key, value)
+        db.session.commit()
+        return UserSchema().dump(existing_user), 201
+    
+    abort(404, f"User not found for ID: {id}")
+    
+
+def delete(id):
+    existing_user = User.query.get(id)
+    
+    if existing_user:
+        db.session.delete(existing_user)
+        db.session.commit()
+        return make_response(f"User with ID {id} deleted.", 204)
+    
+    abort(404, f"User not found for ID: {id}")
+    
--- cw2/Project/app.py
+++ cw2/Project/app.py
@@ -4,13 +4,12 @@ import config
 
 app = config.connex_app
 flask_app = app.app
-# app.add_api(config.basedir / "swagger.yml")
+app.add_api(config.basedir / "swagger.yml")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    print(flask_app.config["SQLALCHEMY_DATABASE_URI"])
     app.run(host="0.0.0.0", port=8000, debug=True)
     
