6d463a3d3011f6fa3e296944af4ae43d1a13d225
Corey Richardson <richardsoncorey04@gmail.com>
2024-12-30 16:37:40 +0000

    Fixes to `models.py` to remove circular references between `User` and `Trail`

--- cw2/Project/models.py
+++ cw2/Project/models.py
@@ -1,12 +1,10 @@
-from datetime import datetime
+from config import db, ma
 
 # https://marshmallow-sqlalchemy.readthedocs.io/en/latest/
 
 from marshmallow_sqlalchemy import fields
-from marshmallow import validates
-
-from config import db, ma
-
+from marshmallow import fields, validates
+          
 class Point(db.Model):
     __tablename__ = "Point"
     
@@ -18,7 +16,10 @@ class Point(db.Model):
     description = db.Column(db.String(127))
     
     next_point = db.relationship(
-        "Point", remote_side = [id], foreign_keys = [next_point_id], backref = "previous_point"
+        "Point", remote_side=[id], foreign_keys=[next_point_id], backref="previous_points"
+    )
+    previous_point = db.relationship(
+        "Point", remote_side=[id], foreign_keys=[previous_point_id], backref="next_points"
     )
     
 
@@ -32,20 +33,19 @@ class PointSchema(ma.SQLAlchemyAutoSchema):
     @validates("latitude")
     def validate_latitude(self, value):
         if not -90 <= value <= 90:
-            raise ValueError("Failed Latitude Validation. Failed check: -90 <= {value} <= 90")
+            raise ValueError(f"Failed Latitude Validation. Failed check: -90 <= {value} <= 90")
         
     @validates("longitude")
     def validate_longitude(self, value):
         if not -180 <= value <= 180:
-            raise ValueError("Failed Longitude Validation. Failed check: -180 <= {value} <= 180")
-    
-    
+            raise ValueError(f"Failed Longitude Validation. Failed check: -180 <= {value} <= 180")
+       
 
 class Trail(db.Model):
-    __tablename__ == "Trail"
+    __tablename__ = "Trail"
     
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    author_id = db.Column(db.Integer, db.ForeignKey("User.id"))
+    author_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable = True)
     starting_point_id = db.Column(db.Integer, db.ForeignKey("Point.id"), nullable = True)
     name = db.Column(db.String(255), nullable = False)
     summary = db.Column(db.String(255), nullable = False)
@@ -55,7 +55,7 @@ class Trail(db.Model):
     elevation_gain = db.Column(db.Integer, nullable = False)
     route_type = db.Column(db.String(15), nullable = False)
     
-    author = db.relationship("User", back_populates = "trails")
+    # author = db.relationship("User", back_populates = "trails")
     starting_point = db.relationship("Point", foreign_keys = [starting_point_id])
     
 
@@ -73,16 +73,16 @@ class TrailSchema(ma.SQLAlchemyAutoSchema):
         valid_routes = ["Loop", "Out & back", "Point to point"]
         if value not in valid_routes:
             raise ValueError(f"Failed Route Type Validation: {value}. Must be in {valid_routes}")
-
-
+        
+          
 class User(db.Model):
-    __tablename__ == "User"
+    __tablename__ = "User"
     
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     email = db.Column(db.String(255), nullable = False, unique = True)
     role = db.Column(db.String(6), nullable = False)
     
-    trails = db.relationship("Trail", back_populates = "author")
+    trails = db.relationship(Trail, backref = "author")
     
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
@@ -90,4 +90,3 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
         model = User
         load_instance = True
         sqla_session = db.session
-        
